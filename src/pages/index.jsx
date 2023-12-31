635cf5617f965edd0204335ec63bbd34d2cf341e
KODIGIT <49615013+kodigit1@users.noreply.github.com>
2023-12-31 11:12:28 +0100

    Update index.jsx
    seo change

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,7 @@ import Wrapper from "../layout/wrapper";
 const Home = () => {
   return (
     <Wrapper>
-      <SEO pageTitle={"Softec - Data analytics"} />
+      <SEO pageTitle={"Registratore di cassa online"} />
       <HomeTwo />
     </Wrapper>
   );
