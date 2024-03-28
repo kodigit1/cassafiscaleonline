35f75cab827883ece806f48a2f2ed68b41ce6b72
KODIGIT <49615013+kodigit1@users.noreply.github.com>
2024-03-28 15:36:28 +0100

    Update answer-question-data.js

--- src/data/answer-question-data.js
+++ src/data/answer-question-data.js
@@ -1,9 +1,9 @@
 const answer_question_data = [
   {
     id: 1,
-    question: "Why should I choose Softec?",
-    answer: <>The Softec Shop is built right into your account dashboard, and is accessible
-          immediately after signing up. </> ,
+    question: "Perchè devo scegliere CassaFiscaleOnline?",
+    answer: <>Il nostro Team qualificato è a tua disposizione per scegliere la soluzione 
+  più appropiata per la tua attività. </> ,
     accordion_id: "headingOne",
     collapsed: "collapsed",
     data_bs_target: "#collapseOne",
@@ -12,9 +12,9 @@ const answer_question_data = [
   },
   {
     id: 2,
-    question: "Do I need to change banks??",
-    answer: <>The Softec Shop is built right into your account dashboard, and is accessible
-          immediately after signing up. </> ,
+    question: "Quali sono i vantaggi per il settore Ristorazione?",
+    answer: <>Cassa utilizzabile con qualsiasi dispositivo consente la gestione dell'ordine con cameriere
+      o al tavolo cliente. </> ,
     accordion_id: "headingTwo",
     collapsed: "",
     data_bs_target: "#collapseTwo",
@@ -26,9 +26,9 @@ const answer_question_data = [
   },
   {
     id: 3,
-    question: "How can I order equipment?",
-    answer: <>The Softec Shop is built right into your account dashboard, and is accessible
-    immediately after signing up. </> ,
+    question: "Quali sono i vantaggi per il settore Estetica?",
+    answer: <>Scontrino virtuale, gestione fidelity Card virtuale, Marketing Automation per l'invio di comunicazioni,
+      Gestione Promo e Coupon , Gestione Appuntamenti con notifica al cliente. </> ,
     accordion_id: "headingThree",
     collapsed: "collapsed",
     data_bs_target: "#collapseThree",
@@ -38,9 +38,9 @@ const answer_question_data = [
   },
   {
     id: 4,
-    question: "Do you offer volume discounts?",
-    answer: <>The Softec Shop is built right into your account dashboard, and is accessible
-          immediately after signing up. </> ,
+    question: "Quali sono i vantaggi per il settore Retail?",
+    answer: <>Connettore Amazon/Ebay, scontrino virtuale, gestione Magazzino e punti Vendita, sincronizzazione inventario,
+      Fidelity Card vrtuale,Scontrino Virtuale. </> ,
     accordion_id: "headingFour",
     collapsed: "collapsed",
     data_bs_target: "#collapseFour",
@@ -50,9 +50,9 @@ const answer_question_data = [
   },
   {
     id: 5,
-    question: "How does signing up work?",
-    answer: <>The Softec Shop is built right into your account dashboard, and is accessible
-    immediately after signing up. </> ,
+    question: "Quali sono i vantaggi per il commercio ambulante",
+    answer: <>Cassa sempre attiva anche con uno smartphone, non necessita di collegamento wi-fi anche con il 
+      collegamento 4G del telefono funziona perfettamente. </> ,
     accordion_id: "headingFive",
     collapsed: "collapsed",
     data_bs_target: "#collapseFive",
