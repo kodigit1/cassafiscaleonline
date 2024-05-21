0cfc88bd54bd29797562952269a9471b93212acd
KODIGIT <49615013+kodigit1@users.noreply.github.com>
2024-05-21 12:36:41 +0200

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import SEO from "../common/seo";
 import HomeTwo from "../components/homes/home-2";
 import Wrapper from "../layout/wrapper";
+import ReactGA from 'react-ga4';
 
 const Home = () => {
+  ReactGA.initialize('YOUR_TRACKING_ID');
   return (
     <Wrapper>
       <SEO pageTitle={"Registratore di cassa online"} />
