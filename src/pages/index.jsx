d69aee35fd4f6329c97cee61d6c7e7669c55db76
KODIGIT <49615013+kodigit1@users.noreply.github.com>
2024-05-21 13:34:56 +0200

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import SEO from "../common/seo";
 import HomeTwo from "../components/homes/home-2";
 import Wrapper from "../layout/wrapper";
-import ReactGA from 'react-ga4';
+
 
 const Home = () => {
   
